279dabcf2e30432337aaf190235b8cf46be54566
Maurilio Siqueira <mauriliosiqueira2001@hotmail.com>
2023-09-22 14:05:10 -0300

    Teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,6 @@ public class Program {
     public static void main(String[] args) {
 
         var posicao = new Posicao(3, 5);
-        System.out.println(posicao);
+        System.out.println("Posição: " + posicao);
     }
 }

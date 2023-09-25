322da3b12b77914235523d3fc528e053221d9b2a
Maurilio Siqueira <mauriliosiqueira2001@hotmail.com>
2023-09-25 11:15:51 -0300

    Melhorando a visualizacao do tabuleiro

--- src/xadrez/Partida.java
+++ src/xadrez/Partida.java
@@ -33,8 +33,20 @@ public class Partida {
     }
 
     private void setupInicial() {
-        colocarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+        colocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+
+        colocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
     }
 }
